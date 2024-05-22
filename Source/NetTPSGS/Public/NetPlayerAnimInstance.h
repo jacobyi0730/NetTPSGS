3ed979801dcd27633abec10784f72111ca86d380
PWS <amurorei2000@gmail.com>
2024-05-22 15:25:53 +0900

    Initial commit

---
+++ Source/NetTPSGS/Public/NetPlayerAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NetPlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETTPSGS_API UNetPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY()
+	class ANetTPSGSCharacter* Me;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Net)
+	bool bHasPistol = false;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Net)
+	float Horizontal;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Net)
+	float Vertical;
+};
